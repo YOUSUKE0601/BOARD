cd6deb7b72f3153750ef862ffa4d6f6de7ada4c9
YOUSUKE0601 <tyousuke0601@gmail.com>
2021-05-26 12:59:54 +0000

    削除機能追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
-  resources :boards, only: [:index, :new, :create, :show]
+  resources :boards
 end
 
 

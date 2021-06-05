f581e9aef47ceec34a31cda918c085ead02a9efd
YOUSUKE0601 <tyousuke0601@gmail.com>
2021-06-05 04:22:41 +0000

    ヘッダーメニューの追加

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def header_link_item(name, path)
+    class_name = 'nav-item' #bootstrapデザインに合わせるため必ずつける
+    class_name << ' active' if current_page?(path) #表示するpathと引数のpathが同じかを確かめている
+    
+    content_tag :li, class: class_name do
+      link_to name, path, class: 'nav-link'
+    end
+  end
 end

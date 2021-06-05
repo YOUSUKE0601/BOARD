2e69fabc42f8968ade32ab6b8355b1be373b38d1
YOUSUKE0601 <tyousuke0601@gmail.com>
2021-06-05 05:07:22 +0000

    ユーザーモデルの認証機能設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
+  get 'mypage', to: 'users#me' #mypageにアクセスが来た場合、usersのmeアクションが呼ばれる
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
   root 'home#index'
+  resources :users, only: [:new, :create]
   resources :boards
   resources :comments, only: [:create, :destroy]
 end
